8047ee7b33d997eaa6d43b2729af0eeba85b38a4
Harlan Cleiton <harlancleiton@gmail.com>
2021-08-23 22:46:24 -0300

    refactor(shared): useExecute get title and message from error

--- src/shared/presentational/hooks/useExecute/index.ts
+++ src/shared/presentational/hooks/useExecute/index.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AppError, InternalServerError, NetworkError } from '../../../domain';
+import { AppError } from '../../../domain';
 import { useToast } from '../useToast';
 import { UseCase, ExecuteHandler } from './types';
 
@@ -9,50 +9,20 @@ export function useExecute<UseCaseT extends UseCase>(usecase: UseCaseT) {
 
   const execute = React.useCallback<ExecuteHandler<UseCaseT['execute']>>(
     async (...args) => {
-      function getErrorMessage(error: any): string {
-        const errorIsInstanceOfAppError = error instanceof AppError;
-
-        const defaultErrorMessage =
-          !errorIsInstanceOfAppError && new AppError().message;
-
-        const errorMessage = errorIsInstanceOfAppError
-          ? error.message
-          : defaultErrorMessage;
-
-        return errorMessage;
-      }
-
-      function getTitleToastError(error: any): string {
-        const isInternalServerError = error instanceof InternalServerError;
-
-        if (isInternalServerError) {
-          return 'Lamentamos pelo incovinente';
-        }
-
-        const isNetworkError = error instanceof NetworkError;
-
-        if (isNetworkError) {
-          return 'Sem conexão';
-        }
-
-        return 'Verifique os dados e tente novamente';
-      }
-
       try {
         const data = await usecase.execute(...args);
 
         return { status: 'success', data };
-      } catch (error) {
-        const errorMessage = getErrorMessage(error);
-        const titleToastError = getTitleToastError(error);
+      } catch (_error) {
+        const error = _error instanceof AppError ? _error : new AppError();
 
         toast({
           type: 'error',
-          title: titleToastError,
-          message: errorMessage
+          title: error.title,
+          message: error.message
         });
 
-        return { status: 'error', error: errorMessage, data: undefined };
+        return { status: 'error', error: error.message, data: undefined };
       }
     },
     [toast, usecase]
