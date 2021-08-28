099bf09af64f161302a46e2026114d1e2a6d5108
Harlan Cleiton <harlancleiton@gmail.com>
2021-08-27 23:20:18 -0300

    chore: disable react/prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
+    'react/prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'no-console': 'warn',
