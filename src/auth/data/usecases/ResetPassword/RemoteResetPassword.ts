14234d56b77e66976d35ad7ec1940cf9bf22454c
Harlan Cleiton <harlancleiton@gmail.com>
2021-09-11 15:14:27 -0300

    feat(auth): implements exceptionFactory method

--- src/auth/data/usecases/ResetPassword/RemoteResetPassword.ts
+++ src/auth/data/usecases/ResetPassword/RemoteResetPassword.ts
@@ -1,5 +1,13 @@
-import { HttpClient, RemoteTemplateMethod } from '../../../../shared';
-import { HttpMethod } from '../../../../shared/data/protocols/http/types';
+import {
+  AppError,
+  BadRequestError,
+  HttpClient,
+  RemoteTemplateMethod
+} from '../../../../shared';
+import {
+  HttpMethod,
+  HttpResponse
+} from '../../../../shared/data/protocols/http/types';
 import { ResetPassword, ResetPasswordModel } from '../../../domain';
 
 export class RemoteResetPassword
@@ -20,5 +28,22 @@ export class RemoteResetPassword
 
   getMethod(): HttpMethod {
     return 'put';
+  }
+
+  exceptionFactory(error: any): AppError {
+    if (!error.response) {
+      return super.exceptionFactory(error);
+    }
+
+    const response: HttpResponse = error.response;
+
+    if (response.status !== 404) {
+      return super.exceptionFactory(error);
+    }
+
+    return new BadRequestError(
+      'Link expirado',
+      'Por favor, gere outro link e tente novamente'
+    );
   }
 }
