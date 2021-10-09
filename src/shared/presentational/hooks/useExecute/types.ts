b87681fdf4aec1159e55667c8e5cc13ef5b09d13
Harlan Cleiton <harlancleiton@gmail.com>
2021-10-09 10:33:43 -0300

    refactor(shared): rename types

--- src/shared/presentational/hooks/useExecute/types.ts
+++ src/shared/presentational/hooks/useExecute/types.ts
@@ -1,17 +1,15 @@
-export interface Response<T> {
+export interface UseCase {
+  execute(...args): Promise<any>;
+}
+
+export type Awaited<T> = T extends PromiseLike<infer U> ? U : T;
+
+export interface ExecuteResponse<T> {
   status: 'success' | 'error';
   data: T;
   error?: string;
 }
 
-export type UseCaseExecute = (...args) => Promise<any>;
-
-export interface UseCase {
-  execute: UseCaseExecute;
-}
-
-export type Awaited<T> = T extends PromiseLike<infer U> ? U : T;
-
-export type ExecuteHandler<Handler extends UseCaseExecute> = (
+export type ExecuteHandler<Handler extends UseCase['execute']> = (
   ...args
-) => Promise<Response<Awaited<ReturnType<Handler>>>>;
+) => Promise<ExecuteResponse<Awaited<ReturnType<Handler>>>>;
